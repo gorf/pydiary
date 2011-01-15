765823503a41072f231884e6fd232c66654b84fd
liuqing.com@gmail.com <liuqing.com@gmail.com@1c4ac56b-39d8-2d69-c086-2c9cd8a5319e>
2011-01-15 09:44:58 +0000

    改为Ajax评论方式，避免重复提交评论

--- trunk/utils/email.py
+++ trunk/utils/email.py
@@ -1,4 +1,6 @@
 #coding=utf-8
+from threading import Thread
+
 from django.core.mail import send_mail, EmailMessage
 from django.contrib.auth.models import User
 
@@ -10,14 +12,18 @@ This is a mail send by the pylogs system automaticaly, Please do not reply this 
 
 def new_comment_mail(post_title,comment_content):
     '''send a mail to admin when a new comment posted'''
+
     #get the first user mailaddress
     try:
+
         firstuser = User.objects.get(id__exact = 1)
         if User is not None:
             subject = u'Pylogs有新评论等待审核'
             message = u'Pylogs中的文章<b>%s</b>有一条新评等待您的审核.内容为:<br/>%s' % (post_title,comment_content)
             recipient_list= [firstuser.email]    
-            send_html_mail(subject,message,recipient_list)
+            th = Thread(target=send_html_mail,args=(subject,message,recipient_list))
+            th.start()
+            #send_html_mail(subject,message,recipient_list)
     except:pass
     
 def send_html_mail(subject,html_content,recipient_list):
